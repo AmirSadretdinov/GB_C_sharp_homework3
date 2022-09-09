66c1f5f5789a9c28875ab1f0a3c6596a9da18b23
AmirSadretdinov <sadretdinov-amir@mail.ru>
2022-09-09 11:40:18 +0300

    Ready Ex19

---
+++ Ex19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+bool CheckPaliandrom(int arg)
+{   int num = arg;
+    if ((num % 10) == (num % 100000 / 10000) && (num % 100 / 10) == (num % 10000 / 1000))
+    {
+        return true;
+    }
+    else return false;
+}
+System.Console.WriteLine("Введите пятизначное число");
+int input = Convert.ToInt32(Console.ReadLine());
+bool num = CheckPaliandrom(input);
+if (num == true){
+    System.Console.WriteLine("Yes");
+}
+else {
+    System.Console.WriteLine("No");
+}
